28755d63792798946c2c856b4d9a6c796b3cc65c
Nazim Huseynov <nazim.huseynov@cert.gov.az>
2024-01-25 18:00:28 +0400

    added terraform.tfvars and modified main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "kgb-vpc" {
   cidr_block = var.vpc_cidr_block
 
   tags = {
@@ -11,9 +11,9 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id 
-  cidr_block = var.vpc_cidr_block
+resource "aws_subnet" "kgb-subnet-1" {
+  vpc_id = aws_vpc.kgb-vpc.id 
+  cidr_block = var.subnet_cidr_block
   availability_zone = var.avail_zone
 
   tags = {
@@ -21,10 +21,10 @@ resource "aws_subnet" "dev-subnet-1" {
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.dev-vpc.id
+output "kgb-vpc-id" {
+  value = aws_vpc.kgb-vpc.id
 }
 
-output "dev-subnet-id" {
-  value = aws_subnet.dev-subnet-1.id
+output "kgb-subnet-id" {
+  value = aws_subnet.kgb-subnet-1.id
 }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_cidr_block = "10.33.0.0/16"
+avail_zone = "us-west-1b"
+subnet_cidr_block = "10.33.30.0/24"
+env_prefix = "kgb"
